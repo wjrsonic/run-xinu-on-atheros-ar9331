421c09b0ba03dbc894d2bccf8413cbce0a2b2170
cbdsqhdd <cbdsqhdd@gmail.com>
2013-03-16 07:06:16 +0000

    a simpe tty interrupt dispatch function,will debug it more later.

--- xinu/device/tty/ttyInterrupt.c
+++ xinu/device/tty/ttyInterrupt.c
@@ -11,8 +11,8 @@ interrupt ttyInterrupt(void)
 	struct	dentry	*devptr;	/* pointer to devtab entry	*/
 	struct	ttycblk	*typtr;		/* pointer to ttytab entry	*/	
 	struct	uart_csreg *uptr;	/* address of UART's CSRs	*/
-	int32	iir = 0;		/* interrupt identification	*/
-	int32	lsr = 0;		/* line status			*/
+	int32	uart_cs = 0;
+	int32	uart_int = 0;
 
 	/* For now, the CONSOLE is the only serial device */
 
@@ -28,54 +28,21 @@ interrupt ttyInterrupt(void)
 
 	/* Decode hardware interrupt request from UART device */
 
-        /* Check interrupt identification register */
+	uart_int = uptr->uart_int;
+	uart_int &= uptr->uart_int_en;
 
-        iir = uptr->iir;
-        if (iir & UART_IIR_IRQ) {
+	if(uart_int & UART_RX_VALID_INT)
+	{
+		uptr->uart_int |= UART_RX_VALID_INT;
+		ttyInter_in(typtr, uptr);
 		return;
-        }
-
-	/* Decode the interrupt cause based upon the value extracted	*/
-	/* from the UART interrupt identification register.  Clear	*/
-	/* the interrupt source and perform the appropriate handling	*/
-	/* to coordinate with the upper half of the driver		*/
-
-        iir &= UART_IIR_IDMASK;		/* Mask off the interrupt ID */
-        switch (iir) {
-
-	    /* Receiver line status interrupt (error) */
-
-	    case UART_IIR_RLSI:
-		lsr = uptr->lsr;
-		return;
-
-	    /* Receiver data available or timed out */
-
-	    case UART_IIR_RDA:
-	    case UART_IIR_RTO:
-
-		sched_cntl(DEFER_START);
-
-		/* For each char in UART buffer, call ttyInter_in */
-
-		while (uptr->lsr & UART_LSR_DR) { /* while chars avail */
-			ttyInter_in(typtr, uptr);
-                }
-
-		sched_cntl(DEFER_STOP);
-
-		return;
-
-            /* Transmitter output FIFO is empty (i.e., ready for more)	*/
-
-	    case UART_IIR_THRE:
-		lsr = uptr->lsr;  /* Read from LSR to clear interrupt */
+	}
+	if(uart_int & UART_TX_EMPTY_INT)
+	{
+		uptr->uart_int |= UART_TX_EMPTY_INT;
+		uptr->uart_int_en &= (~UART_TX_EMPTY_INT_EN);
 		ttyInter_out(typtr, uptr);
 		return;
+	}
 
-	    /* Modem status change (simply ignore) */
-
-	    case UART_IIR_MSC:
-		return;
-	    }
 }
